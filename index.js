ca1e86d03cb1cab39aadf9b9a6ed0247a72d8d99
dassusovan <dassusovan3@gmail.com>
2022-01-23 04:05:45 +0530

    change files ..

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+const DEFAULT_NAME = 'Testing..'
+
 try {
+
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-are-you');
+    const nameToGreet = core.getInput('who-are-you') || DEFAULT_NAME;
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("Time", time);
-    
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
